7ed260f6d36be00def3d4c3cf944192f71ed04ec
Kevin Mazats <k.mazats@gmail.com>
2021-03-05 18:05:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Router from "/src/classes/Router.js";
-import home from "/src/components/home.js";
-import about from "/src/components/about.js";
-import contact from "/src/components/contact.js";
-import error from "/src/components/error.js";
+import Router from "/vanillaJS-router/src/classes/Router.js";
+import home from "/vanillaJS-router/src/components/home.js";
+import about from "/vanillaJS-router/src/components/about.js";
+import contact from "/vanillaJS-router/src/components/contact.js";
+import error from "/vanillaJS-router/src/components/error.js";
 
 const selector = "router-link";
 const app = document.getElementById("app");
 const routes = {
-	"/": home.init(),
-	"/index.html": home.init(),
-	"/contact": contact.init(),
-	"/about": about.init(),
-	"/error": error.init(),
+	"/vanillaJS-router/": home.init(),
+	"/vanillaJS-router/index.html": home.init(),
+	"/vanillaJS-router/contact": contact.init(),
+	"/vanillaJS-router/about": about.init(),
+	"/vanillaJS-router/error": error.init(),
 };
 const router = new Router(app, selector, routes);
